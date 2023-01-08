3efe8bcbbef61b75075cdb35c81c5a96e59b0597
사공지은 <gonii0821@naver.com>
2023-01-08 22:13:14 +0900

    bi-direction between FE and BE ✌️

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,1 +1,20 @@
-alert("hi");
+// 서버와 연결
+const socket = new WebSocket(`ws://${window.location.host}`);
+
+/* 각종 listner 등록 */
+
+socket.addEventListener("open", () => {
+  console.log("Connected to Server ✅");
+});
+
+socket.addEventListener("message", (message) => {
+  console.log("New message : ", message.data);
+});
+
+socket.addEventListener("close", () => {
+  console.log("Disconnected from Server ❎");
+});
+
+setTimeout(() => {
+  socket.send("hello from the browser!!");
+}, 10000);
--- src/server.js
+++ src/server.js
@@ -1,3 +1,5 @@
+import http from "http";
+import WebSocket from "ws";
 import express from "express";
 
 const app = express();
@@ -7,6 +9,26 @@ app.set("view engine", "pug");
 app.set("views", __dirname + "/views");
 app.use("/public", express.static(__dirname + "/public"));
 app.get("/", (req, res) => res.render("home"));
+app.get("/*", (req, res) => res.redirect("/"));
 
 const handleListen = () => console.log(`Listening on http://localhost:3000`);
-app.listen(3000, handleListen); // 포트 설정
+
+// http, ws 서버 둘다 돌리기 가능
+const server = http.createServer(app); // express.js로 http 서버 생성
+const wss = new WebSocket.Server({ server }); // http 서버 위에 ws 서버 생성
+
+/**
+ *
+ * @param socket = 연결된 브라우저
+ */
+wss.on("connection", (socket) => {
+  console.log("Connected to Browser ✅");
+
+  socket.on("close", () => console.log("Disconnected from the Browser ❎"));
+  socket.on("message", (message) => {
+    console.log(message.toString("utf8"));
+  });
+  socket.send("hello!");
+});
+
+server.listen(3000, handleListen);
