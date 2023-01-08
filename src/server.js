cef237c754867e3082a3379b9daebc834392ab06
사공지은 <gonii0821@naver.com>
2023-01-08 19:05:09 +0900

    frontend setup 🦿

--- src/server.js
+++ src/server.js
@@ -2,6 +2,11 @@ import express from "express";
 
 const app = express();
 
-console.log("hello");
+// views 설정 및 render
+app.set("view engine", "pug");
+app.set("views", __dirname + "/views");
+app.use("/public", express.static(__dirname + "/public"));
+app.get("/", (req, res) => res.render("home"));
 
-app.listen(3000); // 포트 설정
+const handleListen = () => console.log(`Listening on http://localhost:3000`);
+app.listen(3000, handleListen); // 포트 설정
